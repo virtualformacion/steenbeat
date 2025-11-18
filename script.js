5535f6e9aa324d52772ebdcd750ab5e042b4017f
virtualformacion <virtualformacion47@gmail.com>
2025-11-18 02:25:36 -0500

    Actualizar usuarios (delete) desde admin

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const USERS = [
     { username: "dameix", password: "2323beat2323", expiresAt: new Date("2025-12-05") },
     { username: "braxei", password: "2323beat2323", expiresAt: new Date("2025-11-30") },
-    { username: "toncasdj", password: "2323beat2323", expiresAt: new Date("2025-11-15") },
     { username: "bobacx", password: "2323beat2323", expiresAt: new Date("2025-12-10") },
     { username: "trcsnn", password: "2323beat2323", expiresAt: new Date("2025-11-20") },
     { username: "mrass", password: "2323beat2323", expiresAt: new Date("2025-11-20") },
