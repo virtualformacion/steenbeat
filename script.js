c1fb1e164e421edf52595785f69f46d269322fa6
virtualformacion <virtualformacion47@gmail.com>
2026-03-29 20:27:00 -0500

    Actualizar usuarios (edit) desde admin

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ========== USUARIOS AUTORIZADOS ==========
 const USERS = [
-    { username: "dameix", password: "2323beat2323", expiresAt: new Date("2026-03-30") },
+    { username: "dameix", password: "2323beat2323", expiresAt: new Date("2027-04-30") },
     { username: "braxei", password: "2323beat2323", expiresAt: new Date("2026-03-30") },
     { username: "trcsnn", password: "2323beat2323", expiresAt: new Date("2026-03-30") },
     { username: "mrass", password: "2323beat2323", expiresAt: new Date("2026-03-30") },
